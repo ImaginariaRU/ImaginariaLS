8c0d7f333386a29eb4e827bf8a2d9633657490b8
Karel Wintersky <karel.wintersky@yandex.ru>
2018-11-03 18:04:54 +0300

    2.5 removed some modules

--- plugins/blacklist/dump2.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `prefix_blacklist`
-  ADD `service` int(11) unsigned NOT NULL;
-
-ALTER TABLE `prefix_blacklist`
-  DROP PRIMARY KEY;
-
-ALTER TABLE `prefix_blacklist`
-  ADD PRIMARY KEY (`content`, `type`, `service`);
--- plugins/recaptcha/templates/skin/default/js/script.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-	setTimeout(
-		function() {
-			$('#registration-user-captcha').html($('#popup-registration-captcha').clone(true,true));
-		},2000);
-});
